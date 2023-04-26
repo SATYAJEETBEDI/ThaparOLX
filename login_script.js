fc730e360cc3fddcbf23f99e7f4ef976c0a80eee
Satyajeet Bedi <satyajeetbedi145@gmail.com>
2023-04-27 00:21:53 +0530

    contact page added

--- login_script.js
+++ login_script.js
@@ -1,6 +1,7 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+const login_btn =document.querySelector("#login");
 
 sign_up_btn.addEventListener('click', () =>{
     container.classList.add("sign-up-mode");
@@ -9,3 +10,7 @@ sign_up_btn.addEventListener('click', () =>{
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
 });
+
+login_btn.addEventListener('click',()=>{
+    window.location.href = "index.html";
+})
